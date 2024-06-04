51bee5902da4a0ccf39c22fca0b8e182568faa7f
Gerard Albajar <gerard.albajar@gmail.com>
2024-06-04 15:03:58 +0200

    Add globalStyles.tsx and styles.css

---
+++ src/styles/globalStyles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  h1 {
+    color: #3878ae;
+    margin: 0;
+  }
+`;
+
+export const AppStyled = styled.div`
+  padding: 20px;
+`
