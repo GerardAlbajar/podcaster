fdf30249c1c15790017faf03f735ebb798105213
Gerard Albajar <gerard.albajar@gmail.com>
2024-06-04 15:07:07 +0200

    Add usePodcastDetail.ts at hooks folder

---
+++ src/hooks/usePodcastDetail.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { PodcastEpisode } from '../services/interfaces';
+import { fetchPodcastDetail } from '../services/api';
+
+const usePodcastDetail = () => {
+  const { podcastId } = useParams<Record<string, string>>();
+  const [episodes, setEpisodes] = useState<PodcastEpisode[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [podcastDetail, setPodcastDetail] = useState<Partial<PodcastEpisode>>({
+    artistName: "",
+    artworkUrl600: "",
+    collectionName: "",
+    description: ""
+  });
+
+  useEffect(() => {
+    const getDetail = async () => {
+      try {
+        if (!podcastId) return;
+        const data = await fetchPodcastDetail(podcastId);
+        const foundEpisode = data.results.find(episode => episode.wrapperType === "track");
+        if (foundEpisode) setPodcastDetail(foundEpisode);
+        setEpisodes(data.results.filter(episode => episode.wrapperType === "podcastEpisode"));
+      } catch (error) {
+        setError('Failed to fetch podcast detail');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getDetail();
+  }, [podcastId]);
+
+  return { podcastDetail, episodes, loading, error };
+};
+
+export default usePodcastDetail;
