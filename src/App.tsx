dba05abe33afec4c2d7852d7b4eb9999e0ea8a6a
Gerard Albajar <gerard.albajar@gmail.com>
2024-06-04 15:02:46 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-function App() {
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import CardListPage from './pages/CardListPage';
+import { AppStyled } from './styles/globalStyles';
+import { Toaster } from 'react-hot-toast';
 
+const App: React.FC = () => {
   return (
-    <>
-    </>
-  )
-}
+    <AppStyled>
+        <Routes>
+            <Route path="/" element={<CardListPage />} />
+        </Routes>
+        <Toaster />
+    </AppStyled>
+  );
+};
 
-export default App
+export default App;
