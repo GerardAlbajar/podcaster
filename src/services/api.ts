ad456da5d3839c94675637d919c703655b319e88
Gerard Albajar <gerard.albajar@gmail.com>
2024-06-04 15:06:46 +0200

    Add fetchPodcastDetail at api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Entry } from './interfaces';
+import { Entry, PodcastDetail } from './interfaces';
 import toast from 'react-hot-toast';
 
 
@@ -11,6 +11,26 @@ export const fetchTopPodcasts = async (): Promise<{ feed: { entry: Entry[] } }> 
         return data;
     } catch (error) {
         toast.error("Error fetching podcast Top Podcasts list");
+        throw error;
+    }
+};
+
+export const fetchPodcastDetail = async (podcastId: string): Promise<PodcastDetail> => {
+    const DETAIL_URL = `https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=30`;
+    const PROXY_URL = `https://api.allorigins.win/get?url=${encodeURIComponent(DETAIL_URL)}`;
+
+    try {
+        const response = await axios.get(PROXY_URL);
+
+        if (response.status === 200) {
+            const data = JSON.parse(response.data.contents);
+            return data;
+        } else {
+            toast.error('Network response was not ok.');
+            throw new Error('Network response was not ok.');
+        }
+    } catch (error) {
+        toast.error("Error fetching podcast detail");
         throw error;
     }
 };
