8ef00a2ba173ccda88dbe0dbe7895a219d62423e
Gerard Albajar <gerard.albajar@gmail.com>
2024-06-04 15:01:23 +0200

    Add PodcastCardList.tsx

---
+++ src/components/PodcastCardList/PodcastCardList.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const CardListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 20px;
+`;
+
---
+++ src/components/PodcastCardList/PodcastCardList.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CardListContainer } from './PodcastCardList.styles';
+import PodcastCard from '../PodcastCard/PodcastCard';
+
+interface PodcastCardData {
+    id: string;
+    title: string;
+    author: string;
+    imageUrl: string;
+}
+
+interface PodcastCardListProps {
+    podcastsList: PodcastCardData[];
+}
+
+const PodcastCardList: React.FC<PodcastCardListProps> = ({ podcastsList }) => {
+    if (!podcastsList.length) {
+        return <p>No podcasts found</p>
+    }
+    return (
+        <CardListContainer>
+            {podcastsList.map((card) => (
+                <PodcastCard
+                    key={card.id}
+                    id={card.id}
+                    title={card.title}
+                    author={card.author}
+                    imageUrl={card.imageUrl}
+                />
+            ))}
+        </CardListContainer>
+    );
+};
+
+export default PodcastCardList;
