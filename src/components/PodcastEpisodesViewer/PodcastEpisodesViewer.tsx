1fa83696b98c17246ffa431463daf73148086de0
Gerard Albajar <gerard.albajar@gmail.com>
2024-06-04 16:45:14 +0200

    Update PodcastEpisodesViewer adding millisToMinutesAndSeconds

--- src/components/PodcastEpisodesViewer/PodcastEpisodesViewer.tsx
+++ src/components/PodcastEpisodesViewer/PodcastEpisodesViewer.tsx
@@ -9,49 +9,55 @@ interface PodcastEpisodesViewerProps {
 }
 
 const PodcastEpisodesViewer: React.FC<PodcastEpisodesViewerProps> = ({ episodes }) => {
-  const { podcastId, episodeId } = useParams<Record<string, string>>();
-  const [selectedEpisode, setSelectedEpisode] = useState<PodcastEpisode | null>(null);
-  const navigate = useNavigate();
+    const { podcastId, episodeId } = useParams<Record<string, string>>();
+    const [selectedEpisode, setSelectedEpisode] = useState<PodcastEpisode | null>(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (episodeId && episodes?.length) {
-      const foundEpisode = episodes.find(episode => episode.trackId.toString() === episodeId);
-      setSelectedEpisode(foundEpisode || null);
+    const millisToMinutesAndSeconds = (millis: number) => {
+        const minutes = Math.floor(millis / 60000);
+        const seconds = ((millis % 60000) / 1000).toFixed(0) as any;
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
     }
-  }, [episodeId, episodes]);
 
-  const handleClick = (episodeId: number) => {
-    navigate(`/podcast/${podcastId}/episode/${episodeId}`);
-  };
+    useEffect(() => {
+        if (episodeId && episodes?.length) {
+        const foundEpisode = episodes.find(episode => episode.trackId.toString() === episodeId);
+        setSelectedEpisode(foundEpisode || null);
+        }
+    }, [episodeId, episodes]);
 
-  if (episodeId && selectedEpisode) {
-    return <EpisodeDetail episode={selectedEpisode} />;
-  }
+    const handleClick = (episodeId: number) => {
+        navigate(`/podcast/${podcastId}/episode/${episodeId}`);
+    };
 
-  return (
-    <Table>
-      <thead>
-        <tr>
-          <TableHeader>Title</TableHeader>
-          <TableHeader>Date</TableHeader>
-          <TableHeader>Duration</TableHeader>
-        </tr>
-      </thead>
-      <tbody>
-        {episodes.map((episode, index) => (
-            <TableRow
-                key={episode.trackId}
-                style={{ backgroundColor: index % 2 !== 0 ? '#f9f9f9' : '#fff' }}
-                onClick={() => handleClick(episode.trackId)}
-            >
-                <TableCell><p>{episode.trackName}</p></TableCell>
-                <TableCell>{new Date(episode.releaseDate).toLocaleDateString()}</TableCell>
-                <TableCell>-</TableCell>
-            </TableRow>
-        ))}
-      </tbody>
-    </Table>
-  );
+    if (episodeId && selectedEpisode) {
+        return <EpisodeDetail episode={selectedEpisode} />;
+    }
+
+    return (
+        <Table>
+        <thead>
+            <tr>
+            <TableHeader>Title</TableHeader>
+            <TableHeader>Date</TableHeader>
+            <TableHeader>Duration</TableHeader>
+            </tr>
+        </thead>
+        <tbody>
+            {episodes.map((episode, index) => (
+                <TableRow
+                    key={episode.trackId}
+                    style={{ backgroundColor: index % 2 !== 0 ? '#f9f9f9' : '#fff' }}
+                    onClick={() => handleClick(episode.trackId)}
+                >
+                    <TableCell><p>{episode.trackName}</p></TableCell>
+                    <TableCell>{new Date(episode.releaseDate).toLocaleDateString()}</TableCell>
+                    <TableCell>{episode.trackTimeMillis ? millisToMinutesAndSeconds(episode.trackTimeMillis) : '-'}</TableCell>
+                </TableRow>
+            ))}
+        </tbody>
+        </Table>
+    );
 };
 
 export default PodcastEpisodesViewer;
